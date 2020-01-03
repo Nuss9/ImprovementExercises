c822ae04db098aa7ba8e6263e9ab831d2747831f
Dennis Jakupovic <djakupovic@winbase.nl>
2020-01-03 16:26:36 +0100

    Add start of Bowling Game kata

--- Solution/BowlingGame.cs
+++ Solution/BowlingGame.cs
@@ -4,6 +4,23 @@ namespace Solution
 {
 	public class Game
 	{
+		private int currentRoll = 0;
+		private int[] rolls = new int[21];
 
+		public void Roll(int pins)
+		{
+			rolls[currentRoll++] = pins;
+		}
+
+		public int Result()
+		{
+			int score = 0;
+
+			for(int i =0; i < rolls.Length; i++) {
+				score += rolls[i];
+			}
+
+			return score;
+		}
 	}
 }
--- SolutionTests/BowlingGameTests.cs
+++ SolutionTests/BowlingGameTests.cs
@@ -1,10 +1,44 @@
 using System;
 using Solution;
+using Xunit;
 
 namespace SolutionTests
 {
 	public class BowlingGameTests
 	{
+		public Game game = new Game();
 
+		[Fact]
+		public void WhenThrowingAllGutters_ItShouldScore0()
+		{
+			RollMany(20, 0);
+
+			Assert.Equal(0, game.Result());
+		}
+
+		[Fact]
+		public void WhenThrowingAllOnes_ItShouldScore20()
+		{
+			RollMany(20, 1);
+
+			Assert.Equal(20, game.Result());
+		}
+
+		[Fact]
+		public void WhenThrowing553_ItShouldScore16()
+		{
+			RollMany(2, 5);
+			RollMany(1, 3);
+			RollMany(17, 0);
+
+			Assert.Equal(16, game.Result());
+		}
+
+		private void RollMany(int rolls, int pins)
+		{
+			for(int i = 0; i < rolls; i++) {
+				game.Roll(pins);
+			}
+		}
 	}
 }
